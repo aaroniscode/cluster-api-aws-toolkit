c4fd7e7841555ece665f51c3e63d8b990c381625
Aaron Miller <milleraa@vmware.com>
2019-12-03 08:32:54 -0700

    small tweaks

--- v1alpha2/03-capa-manifests/templates/get_kubeconfig.sh
+++ v1alpha2/03-capa-manifests/templates/get_kubeconfig.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-kubectl get secret ${cluster_name}-kubeconfig -o jsonpath={.data.value} | base64 --decode > ${cluster_name}.kubeconfig
+kubectl get secret ${cluster_name}-kubeconfig -o jsonpath={.data.value} \
+  | base64 --decode > ${cluster_name}.kubeconfig
+cp ${cluster_name}.kubeconfig ..\04-new-cluster\${cluster_name}.kubeconfig
